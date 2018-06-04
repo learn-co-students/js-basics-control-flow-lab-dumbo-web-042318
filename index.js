cd372201972416c425fbba01c5fd6e7a47879abb
jcr2125 <email>
2018-06-04 16:55:55 -0400

    Done.

--- index.js
+++ index.js
@@ -22,12 +22,9 @@ function switchOnCharmFromTip(tip) {
   switch(tip) {
     case "generous":
       return "Thank you so much."
-      break;
     case "not as generous":
       return "Thank you."
-      break;
     default:
       return "Bye."
-      break;
   }
 }
