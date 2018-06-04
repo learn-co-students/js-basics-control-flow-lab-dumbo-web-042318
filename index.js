2c6dfea1a47eae1bf3e302a0b7b133076f83c7a0
jcr2125 <email>
2018-06-04 16:54:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 400 && someValue <= 2000) {
+    return "This cost varies.";
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(city) {
+  let check;
+  city === "NYC" ? check = "Ok, sounds good." : check = "No go.";
+  return check;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
+}
